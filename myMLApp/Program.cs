d4f0b71a127ded6cc2c05db0eb9f3963302ae033
Daerendal <daerendal@gmail.com>
2022-11-20 00:08:05 +0100

    Dodaj pliki projektów.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyMLApp;
+
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = "This restaurant was wonderful"
+    
+};
+
+var result = SentimentModel.Predict(sampleData);
+
+var sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
